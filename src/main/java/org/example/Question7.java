2a3e581901bbdff10fc6330db2d0558aaa985ec8
Tushar Gupta <77291819+tushar-corbic@users.noreply.github.com>
2024-07-17 18:32:44 +0530

    Create Question7.java

---
+++ src/main/java/org/example/Question7.java
@@ -0,0 +1,19 @@
+1302. Deepest Leaves Sum
+
+Given the root of a binary tree, return the sum of values of its deepest leaves.
+
+
+   public int deepestLeavesSum(TreeNode root) {
+        int res = 0, i;
+        LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            for (i = q.size() - 1, res = 0; i >= 0; --i) {
+                TreeNode node = q.poll();
+                res += node.val;
+                if (node.right != null) q.add(node.right);
+                if (node.left  != null) q.add(node.left);
+            }
+        }
+        return res;
+    }
